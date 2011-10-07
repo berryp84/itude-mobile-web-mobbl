c5fbc16295a8ce6d77bb25e407d5520393b4ddd1
Cristan Meijer <c.meijer@itude.com>
2011-10-07 08:46:49 +0000

    The InitialBean isn't abused for search purposes anymore

--- core-lib/src/main/java/com/itude/mobile/template/jsf/InitialBean.java
+++ core-lib/src/main/java/com/itude/mobile/template/jsf/InitialBean.java
@@ -3,28 +3,16 @@ package com.itude.mobile.template.jsf;
 import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
-import javax.faces.event.ActionEvent;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import com.itude.mobile.mobbl2.client.core.controller.MBOutcome;
-import com.itude.mobile.mobbl2.client.core.model.MBDocument;
-import com.itude.mobile.mobbl2.client.core.services.MBDataManagerService;
-import com.itude.mobile.template.annotations.HttpParam;
 import com.itude.mobile.template.controllers.ApplicationController;
 
-@Named(value = "initialBean")
+@Named
 @SessionScoped
 public class InitialBean implements Serializable
 {
   private static final long     serialVersionUID = 1L;
-
-  @Inject
-  @HttpParam("searchquery")
-  private String                _searchquery;
-
-  @Inject
-  private MBDataManagerService  _dataManagerService;
 
   @Inject
   private ApplicationController _applicationController;
@@ -41,24 +29,4 @@ public class InitialBean implements Serializable
     }
     return _initialized;
   }
-
-  public void setSearchQuery(String _searchquery)
-  {
-    this._searchquery = _searchquery;
-  }
-
-  public String getSearchQuery()
-  {
-    return _searchquery;
-  }
-
-  public void search(ActionEvent av)
-  {
-    MBDocument searchDoc = _dataManagerService.loadDocument("SearchDocument");
-    searchDoc.setValue(_searchquery, "Query[0]/@text()");
-
-    MBOutcome outcome = new MBOutcome("OUTCOME-page_search", searchDoc);
-    _applicationController.handleOutcome(outcome);
-  }
-
 }
---
+++ core-web/src/main/java/com/itude/mobile/mcds/jsf/SearchBean.java
@@ -0,0 +1,46 @@
+package com.itude.mobile.mcds.jsf;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.itude.mobile.mobbl2.client.core.controller.MBOutcome;
+import com.itude.mobile.mobbl2.client.core.model.MBDocument;
+import com.itude.mobile.mobbl2.client.core.services.MBDataManagerService;
+import com.itude.mobile.template.controllers.ApplicationController;
+
+@Named
+@RequestScoped
+public class SearchBean implements Serializable
+{
+  private static final long serialVersionUID = 1L;
+
+  private String                _searchquery;
+
+  @Inject
+  private MBDataManagerService  _dataManagerService;
+  
+  @Inject
+  private ApplicationController _applicationController;
+  
+  public void setSearchQuery(String searchquery)
+  {
+    _searchquery = searchquery;
+  }
+
+  public String getSearchQuery()
+  {
+    return _searchquery;
+  }
+
+  public void search()
+  {
+    MBDocument searchDoc = _dataManagerService.loadDocument("SearchDocument");
+    searchDoc.setValue(_searchquery, "Query[0]/@text()");
+
+    MBOutcome outcome = new MBOutcome("OUTCOME-page_search", searchDoc);
+    _applicationController.handleOutcome(outcome);
+  }
+}
