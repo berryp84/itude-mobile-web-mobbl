6fdc890f4f7f9e4837701ca964fdeb0315511855
Cristan Meijer <c.meijer@itude.com>
2013-02-25 12:56:44 +0100

    Formatted the code and added a e.printStackTrace()

--- core-lib/src/main/java/com/itude/mobile/web/jsf/ErrorComponent.java
+++ core-lib/src/main/java/com/itude/mobile/web/jsf/ErrorComponent.java
@@ -7,33 +7,41 @@ import javax.faces.component.UIComponentBase;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 
-public class ErrorComponent extends UIComponentBase {
+public class ErrorComponent extends UIComponentBase
+{
 
-	@Override
-	public String getFamily() {
-		return "MOBBL";
-	}
+  @Override
+  public String getFamily()
+  {
+    return "MOBBL";
+  }
 
-	@Override
-	public void encodeAll(FacesContext context) throws IOException {
-		ResponseWriter original = context.getResponseWriter();
-		try {
-			StringWriter result = new StringWriter();
-			ResponseWriter clonedWriter = context.getResponseWriter()
-					.cloneWithWriter(result);
-			context.setResponseWriter(clonedWriter);
-			super.encodeAll(context);
-			clonedWriter.flush();
-			original.write(result.getBuffer().toString());
-		} catch (Exception e) {
-			handleError(e, original);
-		} finally {
-			context.setResponseWriter(original);
-		}
-	}
+  @Override
+  public void encodeAll(FacesContext context) throws IOException
+  {
+    ResponseWriter original = context.getResponseWriter();
+    try
+    {
+      StringWriter result = new StringWriter();
+      ResponseWriter clonedWriter = context.getResponseWriter().cloneWithWriter(result);
+      context.setResponseWriter(clonedWriter);
+      super.encodeAll(context);
+      clonedWriter.flush();
+      original.write(result.getBuffer().toString());
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+      handleError(e, original);
+    }
+    finally
+    {
+      context.setResponseWriter(original);
+    }
+  }
 
-	protected void handleError(Exception e, ResponseWriter writer)
-			throws IOException {
-		writer.writeText("Error occurred: " + e.getMessage(), null);
-	}
+  protected void handleError(Exception e, ResponseWriter writer) throws IOException
+  {
+    writer.writeText("Error occurred: " + e.getMessage(), null);
+  }
 }
